51a12d28218d461a92dd5289b8fb69e8aeb86fb4
Jordan Boudreau <jordboudreau@gmail.com>
2019-02-10 22:40:59 -0800

    add markup for tweet and tweet feed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -16,32 +16,68 @@ const Profile = () => {
   )
 }
 const Feed = () => {
-  return (<h2>I am Feed</h2>)
+  return (
+    <div>
+      <div className='container'>
+        <div className='row'>
+          <div className="card">
+            <h5 className="card-header">I am Feed</h5>
+            <Tweet/>
+            <Tweet/>
+            <Tweet/>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 const Tweet = () => {
   return (
-  <form>
-    <div className="form-group">
-      <label for="exampleFormControlTextarea1">Put profile pic here</label>
-      <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="What's on your mind ol' sport?"></textarea>
-      <button className="btn btn-primary" type="submit">Tweet!</button>
+    <div className='container' >
+      <div className="card">
+        
+        <div className="card-body">
+          <h5 className="card-title">I am name of tweeter</h5>
+          <p className="card-text">When George Washington founded a little startup we've come to know as these United States of America, and he was tired of getting shit from his CEO, the King of England, did he give up? No. He said, "Avast, ye fellow badasses, let's build this country the way we motherfucking want to."</p>
+          <a href="#" >Retweet</a>
+          <a href="#" > Favorite</a>
+          <a href="#" > Reply</a>
+        </div>
+      </div>
     </div>
-  </form>
   )
 }
 const TweetForm = () => {
-  return (<h2>I am TweetForm</h2>)
+  return (
+    <form>
+      <div className="form-group">
+        <label for="exampleFormControlTextarea1">Put profile pic here</label>
+        <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="What's on your mind ol' sport?"></textarea>
+        <button className="btn btn-primary" type="submit">Tweet!</button>
+      </div>
+    </form>
+  )
 }
 
 class App extends Component {
   render() {
     return (
       <div>
-      <h1>y u no look like twitter</h1>
-      <Profile/>
-      <TweetForm/>
-      <Feed/>
-      <Tweet/>
+      <h1>okay u kinda look like twitter</h1>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Profile/>
+          </div>
+          <div className="col-6">
+            <TweetForm/>
+            <Feed/>
+          </div>
+          <div className="col">
+          
+          </div>
+        </div>
+      </div>
       </div>
     );
   }
