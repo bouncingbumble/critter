ddf55cde604a49fe6f6d9cf0d46cafe976e0a63b
Jordan Boudreau <jordboudreau@gmail.com>
2019-02-10 21:47:17 -0800

    add component method stubs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const Profile = () => {
+  return (<h2>I am user profile</h2>)
+}
+const Feed = () => {
+  return (<h2>I am Feed</h2>)
+}
+const Tweet = () => {
+  return (<h2>I am Tweet</h2>)
+}
+const TweetForm = () => {
+  return (<h2>I am TweetForm</h2>)
+}
 
 class App extends Component {
   render() {
     return (
+      <div>
       <h1>y u no look like twitter</h1>
+      <Profile/>
+      <Feed/>
+      <Tweet/>
+      <TweetForm/>
+      </div>
     );
   }
 }
