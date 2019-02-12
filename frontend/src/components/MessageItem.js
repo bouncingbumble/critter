75d5815c096aa7c8d6078ecd1536941d7b50397e
Jordan Boudreau <jordboudreau@gmail.com>
2019-02-12 03:03:19 -0800

    round images

--- frontend/src/components/MessageItem.js
+++ frontend/src/components/MessageItem.js
@@ -4,23 +4,27 @@ import { Link } from 'react-router-dom';
 
 const MessageItem = ({ date, profileImageUrl, text, username, removeMessage, isCorrectUser }) => {
     return (
-        <div className='container' >
-            <div className="card">
-                <div className="card-body">
-                <img src={profileImageUrl} alt={username} height="100" width="100" className="timeline-image"></img>
-                <h5 className="card-title"><Link to ='/'>@{username} &nbsp;</Link></h5>
-                <p>{text}</p>
-                {/* {isCorrectUser && (
-                    <a onClick={removeMessage} className="btn btn-danger pull-right">Delete</a>
-                )} */}
-                <span className="text-muted pull-right">
-                    <Moment className="text-muted" format="Do MMM YYYY">
-                        {date}
-                    </Moment>
-                </span>
-                <a href="#" >Retweet</a>
-                <a href="#" > Favorite</a>
-                <a href="#" > Reply</a>
+        <div className='container card' >
+            <div className="row">
+                <div className="col">
+                    <img src={profileImageUrl} alt={username} height="100" width="100" className="timeline-image"></img>
+                </div>
+                <div className="col">
+                    <div className="card-body">
+                        <h5 className="card-title"><Link to ='/'>@{username} &nbsp;</Link></h5>
+                        <p>{text}</p>
+                        {/* {isCorrectUser && (
+                            <a onClick={removeMessage} className="btn btn-danger pull-right">Delete</a>
+                        )} */}
+                        <span className="text-muted pull-right">
+                            <Moment className="text-muted" format="Do MMM YYYY">
+                                {date}
+                            </Moment>
+                        </span>
+                        <a href="#" >Retweet</a>
+                        <a href="#" > Favorite</a>
+                        <a href="#" > Reply</a>
+                    </div>
                 </div>
             </div>
         </div>
--- frontend/src/components/UserProfile.js
+++ frontend/src/components/UserProfile.js
@@ -3,7 +3,7 @@ import React from "react";
 const UserProfile = ({profileImageUrl, username}) => (
     <div className="col-sm-2">
         <div className="card" style={{"width": "18rem"}}>
-            <img src={profileImageUrl} alt={username} className="img-thumbnail" width="200" height="200" />
+            <img src={profileImageUrl} alt={username} width="200" height="200" />
             <div className="card-body">
                 <h5 className="card-title">{username}</h5>
                 <p className="card-text">This is where the users bio will go, coming soon. . .</p>
